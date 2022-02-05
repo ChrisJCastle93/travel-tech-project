4b832be4dcbe8f153f29837f4bdef06c4b9c4e57
Chris Castle <chrisjcastle93@gmail.com>
2022-02-05 11:59:58 +0100

    done

--- app.js
+++ app.js
@@ -78,8 +78,8 @@ const dbConnection = process.env.MONGODB_URI
 
 const clientP = mongoose
   .connect(
-    "mongodb+srv://chrisjcastle93:dougal22@cluster0.ey3wh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    // process.env.MONGODB_URI,
+    // "mongodb+srv://chrisjcastle93:dougal22@cluster0.ey3wh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+    process.env.MONGODB_URI,
     // dbConnection,
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
